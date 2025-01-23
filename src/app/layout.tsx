c28309e168db5ff1f2be7fe43ce4993ee560cffd
53ningen <gomi2ngen@gmail.com>
2025-01-24 00:42:34 +0900

    add GoogleAnalytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,8 @@ import getMetadata from '@/components/common/Meta'
 import { currentLocale } from '@/i18n/config'
 import { getDictionary } from '@/i18n/dictionaries'
 import { DictionaryProvider } from '@/i18n/hook'
+import { GoogleTagManager } from '@next/third-parties/google'
+import Script from 'next/script'
 import './globals.css'
 
 export async function generateMetadata() {
@@ -11,6 +13,8 @@ export async function generateMetadata() {
   const { title, description } = common
   return getMetadata(title, description)
 }
+
+const gaId = 'G-WGNR1BPQJ8'
 
 export default async function RootLayout({
   children,
@@ -20,6 +24,16 @@ export default async function RootLayout({
   const dictionary = await getDictionary(currentLocale)
   return (
     <html lang='ja'>
+      <GoogleTagManager gtmId='G-WGNR1BPQJ8' />
+      <Script src={`https://www.googletagmanager.com/gtag/js?id=${gaId}`} strategy='afterInteractive' async />
+      <Script id='google-analytics' strategy='afterInteractive'>
+        {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${gaId}');
+        `}
+      </Script>
       <body>
         <DictionaryProvider dictionary={dictionary}>
           <Header />
