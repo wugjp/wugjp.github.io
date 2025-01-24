b9524b446ff94c28dd7814d6b6feceea17a6f22d
53ningen <gomi2ngen@gmail.com>
2025-01-24 18:34:08 +0900

    remove unstable_cache

--- src/actions/costume.ts
+++ src/actions/costume.ts
@@ -1,81 +1,63 @@
-import { CacheTag } from '@/lib/cache'
 import prisma from '@/lib/prisma'
 import { CostumeType } from '@prisma/client'
-import { unstable_cache } from 'next/cache'
 
-export const getCostume = unstable_cache(
-  async (id: number) =>
-    prisma.costume.findUnique({
-      include: {
-        images: {
-          where: {
-            costumeId: id,
-          },
-          orderBy: {
-            displayOrder: 'asc',
-          },
-          take: 1,
+export const getCostume = async (id: number) =>
+  prisma.costume.findUnique({
+    include: {
+      images: {
+        where: {
+          costumeId: id,
         },
-      },
-      where: {
-        id,
-      },
-    }),
-  undefined,
-  { tags: [CacheTag('Costumes')] }
-)
-
-export const listCostumes = unstable_cache(
-  async (costumeType?: CostumeType) =>
-    prisma.costume.findMany({
-      include: {
-        images: {
-          orderBy: {
-            displayOrder: 'asc',
-          },
-          take: 1,
+        orderBy: {
+          displayOrder: 'asc',
         },
+        take: 1,
       },
-      where: {
-        costumeType,
-      },
-      orderBy: {
-        displayOrder: 'asc',
-      },
-    }),
-  undefined,
-  { tags: [CacheTag('Costumes')] }
-)
+    },
+    where: {
+      id,
+    },
+  })
 
-export const listCostumeTweets = unstable_cache(
-  async (costumeId: number) =>
-    prisma.costumeTweet.findMany({
-      where: {
-        costumeId,
-      },
-      orderBy: {
-        tweetId: 'asc',
-      },
-    }),
-  undefined,
-  { tags: [CacheTag('Costumes')] }
-)
-
-export const listYouTubeVideosByCostume = unstable_cache(
-  async (costumeId: number) =>
-    prisma.costumeYouTubeVideo.findMany({
-      include: {
-        video: true,
-      },
-      where: {
-        costumeId,
-      },
-      orderBy: {
-        video: {
-          publishedTimestamp: 'asc',
+export const listCostumes = async (costumeType?: CostumeType) =>
+  prisma.costume.findMany({
+    include: {
+      images: {
+        orderBy: {
+          displayOrder: 'asc',
         },
+        take: 1,
       },
-    }),
-  undefined,
-  { tags: [CacheTag('Costumes')] }
-)
+    },
+    where: {
+      costumeType,
+    },
+    orderBy: {
+      displayOrder: 'asc',
+    },
+  })
+
+export const listCostumeTweets = async (costumeId: number) =>
+  prisma.costumeTweet.findMany({
+    where: {
+      costumeId,
+    },
+    orderBy: {
+      tweetId: 'asc',
+    },
+  })
+
+export const listYouTubeVideosByCostume = async (costumeId: number) =>
+  prisma.costumeYouTubeVideo.findMany({
+    include: {
+      video: true,
+    },
+    where: {
+      costumeId,
+    },
+    orderBy: {
+      video: {
+        publishedTimestamp: 'asc',
+      },
+    },
+  })
--- src/actions/youtube.ts
+++ src/actions/youtube.ts
@@ -1,43 +1,30 @@
-import { CacheTag } from '@/lib/cache'
 import prisma from '@/lib/prisma'
 import { unstable_cache } from 'next/cache'
 
 export const listYouTubeChannels = unstable_cache(() => prisma.youTubeChannel.findMany({}))
 
-export const getYouTubeChannel = unstable_cache(
-  async (id: string) =>
-    prisma.youTubeChannel.findUnique({
-      where: {
-        id,
-      },
-    }),
-  undefined,
-  { tags: [CacheTag('YouTube')] }
-)
+export const getYouTubeChannel = async (id: string) =>
+  prisma.youTubeChannel.findUnique({
+    where: {
+      id,
+    },
+  })
 
-export const listYouTubeVideos = unstable_cache(
-  async (channelId: string, offset: number, limit: number) =>
-    prisma.youTubeVideo.findMany({
-      where: {
-        channelId,
-      },
-      orderBy: {
-        publishedTimestamp: 'asc',
-      },
-      skip: offset,
-      take: limit,
-    }),
-  undefined,
-  { tags: [CacheTag('YouTube')] }
-)
+export const listYouTubeVideos = async (channelId: string, offset: number, limit: number) =>
+  prisma.youTubeVideo.findMany({
+    where: {
+      channelId,
+    },
+    orderBy: {
+      publishedTimestamp: 'asc',
+    },
+    skip: offset,
+    take: limit,
+  })
 
-export const getNumberOfYouTubeVideos = unstable_cache(
-  async (channelId: string) =>
-    prisma.youTubeVideo.count({
-      where: {
-        channelId,
-      },
-    }),
-  undefined,
-  { tags: [CacheTag('YouTube')] }
-)
+export const getNumberOfYouTubeVideos = async (channelId: string) =>
+  prisma.youTubeVideo.count({
+    where: {
+      channelId,
+    },
+  })
